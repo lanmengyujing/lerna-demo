75dde458bec84566171cfd248d3b38fa5e5a9254
Liu Jing <jing.liu@rea-group.com>
2018-05-15 17:07:15 +0800

    feat: Add header component

---
+++ packages/footer/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'button';
+
+const Footer = () => (
+    <div>
+        <div>Hello, This is footer</div>
+        <Button/>
+    </div>
+);
+
+export default Footer;
---
+++ packages/header/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'button';
+
+const Header = () => (
+    <div>
+        <div>Hello, This is header</div>
+        <Button/>
+    </div>
+);
+
+export default Header;
